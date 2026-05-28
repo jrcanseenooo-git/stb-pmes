c22cda475e435af8fd16485b0885449a9a07a10e
canseenooo <jrbcancino27@gmail.com>
2026-05-28 08:32:16 +0800

    Update main.js

--- vue-frontend/src/main.js
+++ vue-frontend/src/main.js
@@ -2,8 +2,11 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
+import Toast from 'vue-toastification'
+import 'vue-toastification/dist/index.css'
 
 const app = createApp(App)
 app.use(createPinia())
 app.use(router)
 app.mount('#app')
+app.use(Toast)

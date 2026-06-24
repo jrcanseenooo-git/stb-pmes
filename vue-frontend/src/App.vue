d5f8cd8655baef6ac8bc0524475e5908b26275dd
canseenooo <jrbcancino27@gmail.com>
2026-06-25 00:28:40 +0800

    Update App.vue

--- vue-frontend/src/App.vue
+++ vue-frontend/src/App.vue
@@ -1,7 +1,16 @@
 <template>
   <RouterView />
+  <ConfirmModal
+    v-bind="confirmState"
+    @confirm="onConfirm"
+    @cancel="onCancel"
+  />
 </template>
 
 <script setup>
 import { RouterView } from 'vue-router'
+import ConfirmModal from '@/components/common/ConfirmModal.vue'
+import { useConfirm } from '@/composables/useConfirm'
+
+const { confirmState, onConfirm, onCancel } = useConfirm()
 </script>
